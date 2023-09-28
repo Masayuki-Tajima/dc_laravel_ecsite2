25feb7fcc627634562c6e838bc164fe131720da0
Masayuki-Tajima <masatajima.work@gmail.com>
2023-09-28 10:59:34 +0900

    productsテーブル作成

--- database/migrations/2023_09_28_100935_create_products_table.php
+++ database/migrations/2023_09_28_100935_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name');
+            $table->integer('price');
+            $table->string('product_image');
+            $table->integer('public_flag');
             $table->timestamps();
         });
     }
