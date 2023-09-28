40d3db02b78ff63830628f9d79b6d146b6bd99ec
Masayuki-Tajima <masatajima.work@gmail.com>
2023-09-28 11:32:06 +0900

    新規登録バリデーションチェック実装

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'min:5', 'regex:/^[0-9a-z]+$/'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^[0-9a-z]+$/'],
         ]);
     }
 
